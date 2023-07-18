4075cdf3ca593609bcf8ef79843a578cf8965a4a
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-07-17 21:57:00 -0500

    setting page -- update and delete store complete

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingForm.tsx
@@ -1,81 +1,137 @@
 "use client";
 
 import { Heading } from "@/components/Heading";
+import { AlertModal } from "@/components/modals/alert-modal";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
+import axios from "axios";
 import { SaveIcon, Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import * as z  from "zod";
+import toast from "react-hot-toast";
+import * as z from "zod";
 
 interface SettingFormProps {
   initialData: Store;
 }
 
 const formSchema = z.object({
-    name: z.string().min(3),
-})
+  name: z.string().min(3),
+});
 
 type SettingFormValues = z.infer<typeof formSchema>;
 
 export const SettingForm: React.FC<SettingFormProps> = ({ initialData }) => {
+  const form = useForm<SettingFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+  const params = useParams();
+  const router = useRouter();
 
-      const form = useForm<SettingFormValues>({
-        resolver: zodResolver(formSchema),
-        defaultValues: initialData
-      })  
+  const [open, setOpen] = useState(false); // alert modal
+  const [loading, setLoading] = useState(false);
 
-    const [open, setOpen] = useState(false); // alert modal
-    const [loading, setLoading] = useState(false);
-
-    const onSubmit = async (values: SettingFormValues) => {
-        console.log(values)
+  const onSubmit = async (values: SettingFormValues) => {
+    // console.log(values)
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, values);
+      router.refresh();
+      toast.success("Store updated successfully");
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
     }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/stores/${params.storeId}`);
+      router.refresh();
+      router.push("/");
+      toast.success("Store deleted successfully");
+      
+    } catch (error) {
+      toast.error("Something went wrong");
+    }finally{
+      setLoading(false);
+      setOpen(false);
+    }
+  }
+
   return (
     <>
+    <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+    />
       <div
         className="
             flex flex-center justify-between
         "
       >
         <Heading title="Settings" description="Manage your store settings" />
-        <Button variant="destructive" size="sm" onClick={() => {}} className="" disabled={loading}>
+        <Button
+          variant="destructive"
+          size="sm"
+          onClick={() => setOpen(true)}
+          className=""
+          disabled={loading}
+        >
           <Trash className="h-4 w-4" />
         </Button>
       </div>
       <Separator />
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
-            <div className="
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div
+            className="
                 grid grid-cols-3 gap-8
-            ">
-                <FormField 
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem >
-                            <FormLabel>Name</FormLabel>
-                            <FormControl>
-                                <Input 
-                                    disabled={loading}
-                                    className=""
-                                    placeholder="Store Name"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-            </div>
-            <Button type="submit" className="ml-auto " disabled={loading}>
-                {/* <SaveIcon className="h-4 w-4 "  /> */}
-                Save Changes
-            </Button>
+            "
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      className=""
+                      placeholder="Store Name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit" className="ml-auto " disabled={loading}>
+            {/* <SaveIcon className="h-4 w-4 "  /> */}
+            Save Changes
+          </Button>
         </form>
       </Form>
     </>
