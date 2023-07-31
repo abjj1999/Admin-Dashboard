112d33f1ade90a33bd168477fca141a752031b76
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-07-31 14:14:18 -0500

    billboard page is complete FE &&&& BE

--- README.md
+++ README.md
@@ -25,3 +25,32 @@
         - copy the connection string that contains the database name, username, password, host, port, and db name
         - paste the connection string in the .env file
 
+## shadcn ui
+    A UI library that is used have already prebuilt components that can be used in the app
+        To install shadcn ui
+            - npx shadcn-ui@latest init
+            - after you answer a couple of questions the app folder layout would look like 
+                    ├── app
+                    │   ├── layout.tsx
+                    │   └── page.tsx
+                    ├── components
+                    │   {├── ui
+                    │   │   ├── alert-dialog.tsx
+                    │   │   ├── button.tsx
+                    │   │   ├── dropdown-menu.tsx
+                    │   │   └── ...} ----> here where the components are
+                    │   ├── main-nav.tsx
+                    │   ├── page-header.tsx
+                    │   └── ...
+                    ├── lib
+                    │   └── utils.ts
+                    ├── styles
+                    │   └── globals.css
+                    ├── next.config.js
+                    ├── package.json
+                    ├── postcss.config.js
+                    ├── tailwind.config.js
+                    └── tsconfig.json
+            - to install a component
+                - npx shadcn-ui@latest add <The Name of the component>
+                    * ex: npx shadcn-ui@latest add button
---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel } from "@/components/ui/dropdown-menu";
+import { BillboardColumn } from "./columns";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Copy, Delete, Edit, MoreHorizontal } from "lucide-react";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import { AlertModal } from "@/components/modals/alert-modal";
+
+interface CellActionProps {
+    data: BillboardColumn;
+
+}
+
+
+export const CellAction: React.FC<CellActionProps> = ({
+    data
+}) => {
+    const router = useRouter();
+    const params = useParams();
+    const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+    const copyToClipboard = (id : string) => {
+        navigator.clipboard.writeText(id);
+        toast.success("Copied to clipboard");
+    }
+    const onDelete = async () => {
+        try {
+          setLoading(true);
+          await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+          router.refresh();
+        //   router.push("/");
+          toast.success("Billboard deleted successfully");
+          
+        } catch (error) {
+          toast.error("Something went wrong");
+        }finally{
+          setLoading(false);
+          setOpen(false);
+        }
+      }
+    return (
+        <>
+        <AlertModal 
+            isOpen={open}
+            onClose={() => setOpen(false)}
+            onConfirm={onDelete}
+            loading={loading}
+            />
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <span className="sr-only">
+                        Open menu
+                    </span>
+                    <MoreHorizontal className="w-4 h-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Action</DropdownMenuLabel>
+                <DropdownMenuItem onClick={() => copyToClipboard(data.id)}>
+                    <Copy className="mr-2 h-4 w-4" />
+                    Copy Id
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                 onClick={() => router.push(`/${params.storeId}/billboards/${data.id}`)}
+                >
+                    <Edit className="mr-2 h-4 w-4" />
+                    Update
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setOpen(true)}>
+                    <Delete className="mr-2 h-4 w-4" />
+                    Delete
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+        </>
+    )
+}
