f928ccd506613ff4a47d6e4280f81da0f884f8b0
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-09-11 00:07:54 -0500

    product created, left off at productForm

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { Heading } from '@/components/Heading';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Plus } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+import React from 'react'
+import { ProductColumn, columns } from './columns';
+import { DataTable } from '@/components/ui/data-table';
+import { ApiList } from '@/components/ui/api-list';
+// import { da } from 'date-fns/locale';
+
+
+interface ProductClientProps {
+  data: ProductColumn[]
+}
+
+const ProductClient: React.FC<ProductClientProps> = ({
+  data
+}) => {
+  const router = useRouter();
+  const params = useParams();
+  // console.log(data)
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Products (${data.length})`}
+          description="Manage your store products"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey='label' columns={columns} data={data}/>
+      <Heading 
+        title= "API"
+        description= "API calls for Products"
+      />
+      <Separator />
+      <ApiList
+        entityName="products"
+        entityIdName="prodcutId"
+      />
+    </>
+  )
+}
+
+export default ProductClient
---
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,50 @@
+import prismadb from "@/lib/prismadb";
+import BillboardClient from "./components/client";
+import { ProductColumn } from "./components/columns";
+import {format} from "date-fns";
+import { formatter } from "@/lib/utils";
+
+const ProductPage = async ({
+    params
+}: {
+    params: {
+        storeId: string;
+    };
+}) => {
+    const products =  await prismadb.product.findMany({
+        where: {
+            storeId: params.storeId,
+        },
+        include: {
+            category: true,
+            color: true,
+            size: true,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
+    const formattedProducts: ProductColumn[] = products.map((item) => ({
+        id: item.id,
+        name: item.name,
+        isFeatured: item.isFeatured,
+        isArchived: item.isArchived,
+        price: formatter.format(item.price.toNumber()),
+        category: item.category.name,
+        color: item.color.value,
+        size: item.size.name,
+
+        createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+      }));
+
+    return ( 
+        <div className="flex-col ">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <BillboardClient data={formattedProducts}/>
+            </div>
+        </div>
+     );
+}
+ 
+export default ProductPage;
